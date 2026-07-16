98c19d4ae6b283bb7f288d5f4fc86580065ad81b
dimirtorres3-web <dimirtorres3@gmail.com>
2026-07-15 21:06:05 -0300

    Add home page route to urlpatterns
    Added a route for the home page using TemplateView.

--- tienda_backend/urls.py
+++ tienda_backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')), # <-- Conexión real con Google
+    path('accounts/', include('allauth.urls')), # <-- Esto activa la conexión real con Google
+    path('', TemplateView.as_view(template_name='index.html'), name='home'), # <-- Esto quita el error Not Found
 ]
